5ce50140904d461a572da29dab3edd683bdd8682
Sanders Lauture <golf1052@msn.com>
2015-12-02 21:53:28 -0500

    Setting up settings
    Added a settings file, async/await everywhere (thanks to TS1.7)

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,27 @@
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 var request = require('request');
 var os = require('os');
-var fs = require('fs');
+var fs = require('q-io/fs');
 var ncp = require('ncp').ncp;
 ncp.limit = 16;
 
+var vsCodeExtDir: string = os.homedir() + '/.vscode/extensions';
+var codeSyncExtDir: string = vsCodeExtDir + '/golf1052.code-sync';
+var codeSyncDir: string;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "code-sync" is now active!');
 		
-	var vsCodeExtensionsDir: string = os.homedir() + '/.vscode/extensions';
-	var codeSyncDir: string = os.homedir() + '/OneDrive/Apps/code-sync';
-	var validExtensions = [];
-	for (var i = 0; i < vscode.extensions.all.length; i++) {
-		var ext = vscode.extensions.all[i];
-		if (ext.extensionPath.startsWith(process.env.USERPROFILE)) {
-			validExtensions.push(ext);
-		}
-	}
-	console.log(validExtensions);
+	await checkForSettings();
+	console.log(getInstalledExtensions());
 	// fs.access(vsCodeExtensionsDir, fs.R_OK, function (err) {
 	// 	console.log(err ? 'no access' : 'can read');
 	// });
@@ -42,20 +39,43 @@ export function activate(context: vscode.ExtensionContext) {
 	// 		console.log('copy completed successfully');
 	// 	}
 	// });
-	
-	// request('', function (error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 		console.log(body);
-	// 	}
-	// });
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	var exportExtensionsDisposable = vscode.commands.registerCommand('extension.exportExtensions', () => {
+	let exportExtensionsDisposable = vscode.commands.registerCommand('extension.exportExtensions', () => {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Extensions Exported!');
 	});
 	
 	context.subscriptions.push(exportExtensionsDisposable);
+}
+
+async function checkForSettings() {
+	if (await fs.exists(codeSyncExtDir) == false) {
+		await fs.makeDirectory(codeSyncExtDir);
+	}
+	if (await fs.exists(codeSyncExtDir + '/settings.json') == false) {
+		let path: string = await vscode.window.showInputBox({
+			prompt: 'Enter the full path to where you want code-sync to sync your extensions',
+			value: os.homedir() + '/OneDrive/Apps/code-sync'
+		});
+		let tmpSettings = {
+			externalPath: path
+		};
+		await fs.write(codeSyncExtDir + '/settings.json', JSON.stringify(tmpSettings));
+	}
+	
+	let settings = JSON.parse(await fs.read(codeSyncExtDir + '/settings.json'));
+	codeSyncDir = settings.externalPath;
+}
+
+function getInstalledExtensions(): vscode.Extension<any>[] {
+	let extensions: vscode.Extension<any>[] = [];
+	vscode.extensions.all.forEach(extention => {
+		if (extention.extensionPath.startsWith(os.homedir())) {
+			extensions.push(extention);
+		}
+	});
+	return extensions;
 }
