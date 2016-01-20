314b40d97fca4fe89b9eed249f162f1f004e9448
Sanders Lauture <golf1052@msn.com>
2016-01-19 21:27:55 -0500

    Added setup and destroy for tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,3 +1,5 @@
+"use strict";
+
 // 
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
@@ -9,8 +11,12 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import * as codesync from '../src/extension';
+import * as codesync from '../src/codesync';
 import * as helpers from '../src/helpers';
+
+var currentVersion = '1.1.0';
+var vsCodeExtensionDir: string = helpers.getHomeDirectory() + '/.vscode/extensions';
+var codeSyncExtensionDir: string = helpers.getHomeDirectory() + '/Desktop/golf1052.code-sync-' + currentVersion;
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
@@ -27,3 +33,12 @@ suite("Extension Tests", () => {
         assert.equal(-1, helpers.isVersionGreaterThan('0.0.0.2', '0.0.1'));
     });
 });
+
+function setUpTest() {
+    var codeSync: codesync.CodeSync = new codesync.CodeSync(currentVersion, vsCodeExtensionDir, codeSyncExtensionDir);
+    return codesync;
+}
+
+async function destroyTest() {
+    await helpers.deleteDirectory(codeSyncExtensionDir);
+}
